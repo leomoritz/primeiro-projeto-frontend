3899134b8dc4556d7050e6ce67767912d52e247f
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-06-03 09:35:52 -0300

    Finalizado exercícios 9 e 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { SigninPageComponent } from './signin-page/signin-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     LoginPageComponent,
+    SigninPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  email: string = "";
+  password: string = "";
+  isRequired: boolean = false;
+  passwordIsValid: boolean = true;
+  regexPassword: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaLogin(){
+    if(this.email == '' || this.password == ''){
+      this.isRequired = true;
+    } 
   }
 
 }
